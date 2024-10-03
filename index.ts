91508374ddd4c3804532288247c142f1347a5c34
Frederic Junod <frederic.junod@camptocamp.com>
2024-10-03 14:00:43 +0200

    Add guess button

--- index.ts
+++ index.ts
@@ -6,8 +6,13 @@ import { createOpenLayersMap } from "./openlayers";
 import CesiumSphereCamera from "@geoblocks/cesium-sphere-camera";
 import "@geoblocks/cesium-compass-bar";
 import { Math as CesiumMath, Cartographic } from "@cesium/engine";
-import { toLonLat } from "ol/proj";
+import { fromLonLat, toLonLat } from "ol/proj";
 import { getDistance } from "ol/sphere";
+import Feature from "ol/Feature";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Point from "ol/geom/Point";
+import { LineString } from "ol/geom";
 
 createCesiumWidget("cesium").then((viewer) => {
   const sphereMode = new CesiumSphereCamera(viewer);
@@ -17,8 +22,56 @@ createCesiumWidget("cesium").then((viewer) => {
   compassBar.scene = viewer.scene;
 
   const map = createOpenLayersMap("openlayers");
+  const clickFeature = new Feature({
+    type: "click",
+  });
+  const cameraFeature = new Feature({
+    type: "camera",
+  });
+  const lineFeature = new Feature({
+    type: "line",
+  });
+
+  map.addLayer(
+    new VectorLayer({
+      source: new VectorSource({
+        features: [clickFeature, cameraFeature, lineFeature],
+      }),
+      style: [
+        {
+          filter: ["==", ["get", "type"], "click"],
+          style: {
+            "circle-radius": 8,
+            "circle-fill-color": "#fff",
+            "circle-stroke-color": "#f00",
+            "circle-stroke-width": 3,
+          },
+        },
+        {
+          filter: ["==", ["get", "type"], "camera"],
+          style: {
+            "circle-radius": 8,
+            "circle-fill-color": "#fff",
+            "circle-stroke-color": "#000",
+            "circle-stroke-width": 3,
+          },
+        },
+        {
+          filter: ["==", ["get", "type"], "line"],
+          style: {
+            "stroke-width": 6,
+            "stroke-color": "#000",
+            "stroke-line-dash": [6, 12],
+          },
+        },
+      ],
+    })
+  );
 
   map.on("click", (event) => {
+    const clickGeometry = new Point(event.coordinate);
+    clickFeature.setGeometry(clickGeometry);
+
     const cartographicCameraPosition = Cartographic.fromCartesian(
       viewer.scene.camera.position
     );
@@ -26,10 +79,17 @@ createCesiumWidget("cesium").then((viewer) => {
       CesiumMath.toDegrees(cartographicCameraPosition.longitude),
       CesiumMath.toDegrees(cartographicCameraPosition.latitude),
     ];
+    const cameraGeometry = new Point(fromLonLat(cameraPosition));
+    cameraFeature.setGeometry(cameraGeometry);
+    lineFeature.setGeometry(
+      new LineString([
+        clickGeometry.getCoordinates(),
+        cameraGeometry.getCoordinates(),
+      ])
+    );
+
     const clickPosition = toLonLat(event.coordinate);
-
     const distance = getDistance(cameraPosition, clickPosition);
-
     console.log(`Distance: ${Math.round(distance)}[m]`);
   });
 });
