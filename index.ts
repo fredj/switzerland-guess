30245a577760f48ece40af2d76658481e30559c3
Frederic Junod <frederic.junod@camptocamp.com>
2025-02-17 15:11:16 +0100

    Result dialog

--- index.ts
+++ index.ts
@@ -1,18 +1,12 @@
 import "@cesium/engine/Source/Widget/CesiumWidget.css";
 
 import { createCesiumWidget } from "./cesium";
-import { createOpenLayersMap } from "./openlayers";
+import GameMap from "./openlayers";
 
 import CesiumSphereCamera from "@geoblocks/cesium-sphere-camera";
 import "@geoblocks/cesium-compass-bar";
 import { Math as CesiumMath, Cartographic } from "@cesium/engine";
-import { fromLonLat, toLonLat } from "ol/proj";
-import { getDistance } from "ol/sphere";
-import Feature from "ol/Feature";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import Point from "ol/geom/Point";
-import { LineString } from "ol/geom";
+import { fromLonLat } from "ol/proj";
 
 import type CesiumCompassBar from "@geoblocks/cesium-compass-bar";
 
@@ -23,56 +17,20 @@ createCesiumWidget("cesium").then((viewer) => {
   const compassBar = document.querySelector<CesiumCompassBar>("cesium-compass-bar");
   compassBar.scene = viewer.scene;
 
-  const map = createOpenLayersMap("openlayers");
-  const clickFeature = new Feature({
-    type: "click",
-  });
-  const cameraFeature = new Feature({
-    type: "camera",
-  });
-  const lineFeature = new Feature({
-    type: "line",
+  const gameMap = new GameMap("map");
+
+  const guessButton = document.querySelector("#guess-button") as HTMLButtonElement;
+  const guessCard = document.querySelector("#guess-card") as any;
+  const resultDialog = document.querySelector('#result-dialog') as any;
+
+  gameMap.map.on("click", (event) => {
+    gameMap.setGuessedPosition(event.coordinate);
+    guessButton.disabled = false;
   });
 
-  map.addLayer(
-    new VectorLayer({
-      source: new VectorSource({
-        features: [clickFeature, cameraFeature, lineFeature],
-      }),
-      style: [
-        {
-          filter: ["==", ["get", "type"], "click"],
-          style: {
-            "circle-radius": 8,
-            "circle-fill-color": "#fff",
-            "circle-stroke-color": "#f00",
-            "circle-stroke-width": 3,
-          },
-        },
-        {
-          filter: ["==", ["get", "type"], "camera"],
-          style: {
-            "circle-radius": 8,
-            "circle-fill-color": "#fff",
-            "circle-stroke-color": "#000",
-            "circle-stroke-width": 3,
-          },
-        },
-        {
-          filter: ["==", ["get", "type"], "line"],
-          style: {
-            "stroke-width": 6,
-            "stroke-color": "#000",
-            "stroke-line-dash": [6, 12],
-          },
-        },
-      ],
-    })
-  );
-
-  map.on("click", (event) => {
-    const clickGeometry = new Point(event.coordinate);
-    clickFeature.setGeometry(clickGeometry);
+  guessButton.addEventListener("click", () => {
+    guessCard.classList.add("hidden");
+    gameMap.map.setTarget('result-map');
 
     const cartographicCameraPosition = Cartographic.fromCartesian(
       viewer.scene.camera.position
@@ -81,17 +39,19 @@ createCesiumWidget("cesium").then((viewer) => {
       CesiumMath.toDegrees(cartographicCameraPosition.longitude),
       CesiumMath.toDegrees(cartographicCameraPosition.latitude),
     ];
-    const cameraGeometry = new Point(fromLonLat(cameraPosition));
-    cameraFeature.setGeometry(cameraGeometry);
-    lineFeature.setGeometry(
-      new LineString([
-        clickGeometry.getCoordinates(),
-        cameraGeometry.getCoordinates(),
-      ])
-    );
+    gameMap.showResult(fromLonLat(cameraPosition));
+    document.querySelector("#distance").value = gameMap.distance;
 
-    const clickPosition = toLonLat(event.coordinate);
-    const distance = getDistance(cameraPosition, clickPosition);
-    console.log(`Distance: ${Math.round(distance)}[m]`);
+    resultDialog.open = true;
   });
+
+  resultDialog.addEventListener('wa-hide', () => {
+    // FIXME: new random position
+    gameMap.map.setTarget('map');
+    gameMap.reset();
+
+    guessButton.disabled = true;
+    guessCard.classList.remove("hidden");
+  });
+
 });
