96729ac4d75141abf07bd36520e16e6a491b233b
Frederic Junod <frederic.junod@camptocamp.com>
2025-10-10 14:33:58 +0200

    feat: add fixes locations

---
+++ locations.ts
@@ -0,0 +1,23 @@
+export const LOCATIONS = {
+  ch: [
+    [7.6586, 45.9763], // Matterhorn
+    [7.9806, 46.5475], // Jungfraujoch
+    [8.3093, 47.0502], // Lucerne
+    [7.8632, 46.6863], // Interlaken
+    [8.5417, 47.3769], // Zurich
+  ],
+  fr: [
+    [2.2945, 48.8584], // Eiffel Tower (Paris)
+    [2.3333, 48.8606], // Louvre Museum (Paris)
+    [7.4225, 43.7034], // Promenade des Anglais (Nice)
+    [2.125, 43.9265], // Mont-Saint-Michel
+    [7.2248, 43.7384], // Palace of Versailles
+  ],
+  de: [
+    [13.405, 52.52], // Brandenburg Gate (Berlin)
+    [11.5761, 48.1372], // Marienplatz (Munich)
+    [10.7526, 49.4429], // Neuschwanstein Castle
+    [7.466, 51.4946], // Cologne Cathedral
+    [7.8986, 47.9961], // Old Town (Freiburg im Breisgau)
+  ],
+};
